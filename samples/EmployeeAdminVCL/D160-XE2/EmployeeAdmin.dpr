b41de7fe0751ecdc56b51502a6e120c1e7ca5876
Jorge L. Cangas <jorge.cangas@gmail.com>
2012-06-20 11:44:38 +0200

    move groupprj

--- samples/EmployeeAdminVCL/D160-XE2/EmployeeAdmin.dpr
+++ samples/EmployeeAdminVCL/D160-XE2/EmployeeAdmin.dpr
@@ -1,25 +1,25 @@
 program EmployeeAdmin;
 
 uses
-  Forms,
+  Vcl.Forms,
+  EmployeeAdmin.Facade in '..\App\EmployeeAdmin.Facade.pas',
   EmployeeAdmin.MainWin in '..\App\EmployeeAdmin.MainWin.pas' {MainWindow},
   EmployeeAdmin.Controller.AddRoleResultCommand in '..\Controller\EmployeeAdmin.Controller.AddRoleResultCommand.pas',
   EmployeeAdmin.Controller.DeleteUserCommand in '..\Controller\EmployeeAdmin.Controller.DeleteUserCommand.pas',
-  EmployeeAdmin.Model.VO.UserVO in '..\Model\VO\EmployeeAdmin.Model.VO.UserVO.pas',
+  EmployeeAdmin.Controller.StartupCommand in '..\Controller\EmployeeAdmin.Controller.StartupCommand.pas',
   EmployeeAdmin.Model.RoleProxy in '..\Model\EmployeeAdmin.Model.RoleProxy.pas',
   EmployeeAdmin.Model.UserProxy in '..\Model\EmployeeAdmin.Model.UserProxy.pas',
-  EmployeeAdmin.Facade in '..\App\EmployeeAdmin.Facade.pas',
-  EmployeeAdmin.Model.Enum.DeptEnum in '..\Model\Enum\EmployeeAdmin.Model.Enum.DeptEnum.pas',
-  EmployeeAdmin.Model.Enum.RoleEnum in '..\Model\Enum\EmployeeAdmin.Model.Enum.RoleEnum.pas',
   EmployeeAdmin.Model.VO.RoleVO in '..\Model\VO\EmployeeAdmin.Model.VO.RoleVO.pas',
-  EmployeeAdmin.Controller.StartupCommand in '..\Controller\EmployeeAdmin.Controller.StartupCommand.pas',
+  EmployeeAdmin.Model.VO.UserVO in '..\Model\VO\EmployeeAdmin.Model.VO.UserVO.pas',
+  EmployeeAdmin.Model.Enum.DeptEnum in '..\Model\Enum\EmployeeAdmin.Model.Enum.DeptEnum.pas',
+  EmployeeAdmin.Model.Enum.Enum in '..\Model\Enum\EmployeeAdmin.Model.Enum.Enum.pas',
+  EmployeeAdmin.Model.Enum.RoleEnum in '..\Model\Enum\EmployeeAdmin.Model.Enum.RoleEnum.pas',
+  EmployeeAdmin.View.RolePanelMediator in '..\View\EmployeeAdmin.View.RolePanelMediator.pas',
   EmployeeAdmin.View.UserFormMediator in '..\View\EmployeeAdmin.View.UserFormMediator.pas',
   EmployeeAdmin.View.UserListMediator in '..\View\EmployeeAdmin.View.UserListMediator.pas',
-  EmployeeAdmin.View.RolePanelMediator in '..\View\EmployeeAdmin.View.RolePanelMediator.pas',
-  EmployeeAdmin.View.Components.UserFrm in '..\View\Components\EmployeeAdmin.View.Components.UserFrm.pas' {UserForm: TFrame},
-  EmployeeAdmin.View.Components.UserLst in '..\View\Components\EmployeeAdmin.View.Components.UserLst.pas' {UserList: TFrame},
   EmployeeAdmin.View.Components.RolePanl in '..\View\Components\EmployeeAdmin.View.Components.RolePanl.pas' {RolePanel: TFrame},
-  EmployeeAdmin.Model.Enum.Enum in '..\Model\Enum\EmployeeAdmin.Model.Enum.Enum.pas';
+  EmployeeAdmin.View.Components.UserFrm in '..\View\Components\EmployeeAdmin.View.Components.UserFrm.pas' {UserForm: TFrame},
+  EmployeeAdmin.View.Components.UserLst in '..\View\Components\EmployeeAdmin.View.Components.UserLst.pas' {UserList: TFrame};
 
 {$R *.res}
 var
